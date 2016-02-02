e5e923983ef6923adf3947a5f1026cc38c2ca9d6
Ian Juma <ian.juma@yahoo.com>
2016-02-02 13:03:15 +0300

    update package info

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'africastalking'
+]
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
-  name = 'africastalking',
-  packages = ['africastalking'], # this must be the same as the name above
-  version = '1.0',
-  description = 'An Official Python library for communicating with the AfricasTalking REST API',
-  author = 'Ian Juma',
-  author_email = 'ijuma@africastalking.com',
-  url = 'https://github.com/AfricasTalkingLtd/africastalking-python',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['ussd', 'voice', 'sms' 'mpesa', 'payments', 'africastalking'],
-  classifiers = [],
+  name='africastalking',
+  packages=['africastalking'], # this must be the same as the name above
+  version='1.0',
+  description='An Official Python library for communicating with the AfricasTalking REST API',
+  long_description=readme + '\n\n',
+  author='Ian Juma',
+  author_email='ijuma@africastalking.com',
+  url='https://github.com/AfricasTalkingLtd/africastalking-python',
+  download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/1.0',
+  keywords=['ussd', 'voice', 'sms' 'mpesa', 'payments', 'africastalking'],
+  classifiers=[],
 )
