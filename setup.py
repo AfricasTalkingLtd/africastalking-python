03f5a3b621ac45e1616e97ce04cc42afe2ffb67b
Ian Juma <ian.juma@yahoo.com>
2016-02-02 13:06:49 +0300

    update package info

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from distutils.core import setup
+import sys, os
+
+from codecs import open
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
@@ -8,19 +11,15 @@ packages = [
     'africastalking'
 ]
 
-with open('README.md', 'r', 'utf-8') as f:
-    readme = f.read()
-
 setup(
-  name='africastalking',
-  packages=['africastalking'], # this must be the same as the name above
-  version='1.0',
-  description='An Official Python library for communicating with the AfricasTalking REST API',
-  long_description=readme + '\n\n',
-  author='Ian Juma',
-  author_email='ijuma@africastalking.com',
-  url='https://github.com/AfricasTalkingLtd/africastalking-python',
-  download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/1.0',
-  keywords=['ussd', 'voice', 'sms' 'mpesa', 'payments', 'africastalking'],
-  classifiers=[],
+    name='africastalking',
+    packages=['africastalking'],  # this must be the same as the name above
+    version='1.0',
+    description='An Official Python library for communicating with the AfricasTalking REST API',
+    author='Ian Juma',
+    author_email='ijuma@africastalking.com',
+    url='https://github.com/AfricasTalkingLtd/africastalking-python',
+    download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/1.0',
+    keywords=['ussd', 'voice', 'sms' 'mpesa', 'payments', 'africastalking'],
+    classifiers=[],
 )
