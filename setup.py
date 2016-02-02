5d198176c1c78bedd94d4aebb00836722f60cc87
Ian Juma <ian.juma@yahoo.com>
2016-02-02 12:21:35 +0300

    - create module AT add setup info

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'africastalking',
+  packages = ['africastalking'], # this must be the same as the name above
+  version = '0.1',
+  description = 'An Official Python library for communicating with the AfricasTalking REST API',
+  author = 'Ian Juma',
+  author_email = 'ijuma@africastalking.com',
+  url = 'https://github.com/AfricasTalkingLtd/africastalking-python',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['ussd', 'voice', 'sms' 'mpesa', 'payments', 'africastalking'],
+  classifiers = [],
+)
