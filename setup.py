314ae6b182beeba9e66f66f871327f2eec16354b
Ian Juma <ian.juma@yahoo.com>
2016-02-02 17:42:45 +0300

    update version and add package

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='AfricastalkingGateway',
-    version='1.3',
+    version='1.4',
+    packages=['africastalking'],
     description='An Official Python library for communicating with the AfricasTalking REST API',
     author='Ian Juma',
     author_email='ijuma@africastalking.com',
     url='https://github.com/AfricasTalkingLtd/africastalking-python',
-    download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/1.3',
+    download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/1.4',
     keywords=['ussd', 'voice', 'sms', 'mpesa', 'payments', 'africastalking'],
     classifiers=[],
 )
