12d119a4f389a1266b8bbb5f569aee269f971210
Ian Juma <ian.juma@yahoo.com>
2016-02-02 13:55:14 +0300

    setup file update

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='AfricastalkingGateway',
-    version='1.2',
+    version='1.3',
     description='An Official Python library for communicating with the AfricasTalking REST API',
     author='Ian Juma',
     author_email='ijuma@africastalking.com',
     url='https://github.com/AfricasTalkingLtd/africastalking-python',
-    download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/1.2',
+    download_url='https://codeload.github.com/AfricasTalkingLtd/africastalking-python/tar.gz/1.3',
     keywords=['ussd', 'voice', 'sms', 'mpesa', 'payments', 'africastalking'],
     classifiers=[],
 )
