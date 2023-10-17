7568f61a0898b93b769e0f739e77db8a25484634
nicholas-ngatia <nmngatia@gmail.com>
2023-10-17 10:17:33 +0300

    switch to schema instead of enum due to python versioning

--- africastalking/Chat.py
+++ africastalking/Chat.py
@@ -1,30 +1,18 @@
-from .Service import ChatService, validate_phone, validate_keys
-from enum import Enum, EnumMeta
+from .Service import ChatService, validate_phone
+from schema import Schema, And, Optional, SchemaError
 
 
-class MyEnumMeta(EnumMeta):
-    def __contains__(cls, item):
-        return item in cls.__members__.values()
+chatTypes = {"whatsapp": "WhatsApp", "telegram": "Telegram"}
 
+messageTypes = {"text": "Text", "media": "Media", "location": "Location"}
 
-class ChatServices(Enum, metaclass=EnumMeta):
-    WHATSAPP = "WhatsApp"
-    TELEGRAM = "Telegram"
-
-
-class MessageType(Enum, metaclass=EnumMeta):
-    TEXT = "Text"
-    MEDIA = "Media"
-    LOCATION = "Location"
-
-
-class MediaType(Enum, metaclass=EnumMeta):
-    IMAGE = "Image"
-    AUDIO = "Audio"
-    VIDEO = "Video"
-    DOCUMENT = "Document"
-    STICKER = "Sticker"
-    VOICE = "Voice"
+mediaTypes = {
+    "image": "Image",
+    "audio": "Audio",
+    "video": "Video",
+    "sticker": "Sticker",
+    "voice": "Voice",
+}
 
 
 class WhatsappService(ChatService):
@@ -35,37 +23,6 @@ class WhatsappService(ChatService):
         super(WhatsappService, self)._init_service()
 
     def send(self, message, product_id, channel_number, customer_number, channel):
-        if not channel in ChatServices:
-            raise ValueError(
-                f"Invalid channel, accepted channels are: {ChatServices._member_names_}"
-            )
-
-        if channel == ChatServices.WHATSAPP:
-            if not validate_phone(customer_number):
-                raise ValueError("Invalid phone number: " + customer_number)
-
-            if not validate_phone(channel_number):
-                raise ValueError("Invalid channel number: " + channel_number)
-
-        if not validate_keys(message, {"type"}) or not message["type"] in MessageType:
-            raise (
-                ValueError(
-                    f"Must specify valid message type: {MessageType._member_names_}"
-                )
-            )
-
-        if message["type"] == MessageType.MEDIA:
-            if (
-                not message["media"]
-                or not message["media"] in MediaType
-            ):
-                raise (
-                    ValueError(
-                        f"Must specify valid media type: {MediaType._member_names_}"
-                    )
-                )
-
-        url = self._make_url("/message/send")
         data = {
             "username": self._username,
             "productId": product_id,
@@ -74,16 +31,42 @@ class WhatsappService(ChatService):
             "customerNumber": customer_number,
             "body": message,
         }
+        messageSchema = Schema(
+            [
+                {
+                    "productId": And(str, len),
+                    "username": And(str, len),
+                    "productId": And(str, len),
+                    "channelNumber": And(str, lambda s: validate_phone(s)),
+                    "channel": And(str, lambda s: s in chatTypes),
+                    "customerNumber": And(str, lambda s: validate_phone(s)),
+                    "body": {
+                        "type": And(str, lambda s: s in messageTypes),
+                        Optional("latitude"): And(float, len),
+                        Optional("longitude"): And(float, len),
+                        Optional("media"): And(str, lambda s: s in mediaTypes),
+                        Optional("url"): And(str, len),
+                        Optional("text"): And(str, len),
+                    },
+                }
+            ]
+        )
+        data = messageSchema.validate(data)
+
+        url = self._make_url("/message/send")
 
         return self._make_request(
             url, "POST", headers=self._headers, params=None, data=data
         )
 
     def consent_response(self, channel_number, customer_number, channel, opt_in):
-        if not channel in ChatServices:
-            raise ValueError(
-                f"Invalid channel, accepted channels are: {ChatServices._member_names_}"
-            )
+        if channel == chatTypes["whatsapp"]:
+            if not validate_phone(customer_number):
+                raise ValueError("Invalid phone number: " + customer_number)
+
+            if not validate_phone(channel_number):
+                raise ValueError("Invalid channel number: " + channel_number)
+
         url = self._make_url("/chat/consent")
         data = {
             "username": self._username,
